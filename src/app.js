08493945b0fd242526a133b62a89f25f0f53b889
budzinska <budzinska.anna@gmail.com>
2021-01-06 19:31:58 +0100

    code ordering

--- src/app.js
+++ src/app.js
@@ -1,37 +1,37 @@
 function displayTemperature(response) {
   console.log(response.data);
-
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.name;
-
-  /*
-  let weatherForecastIconElement = document.querySelector(
-    "#weather-forecast-icon"
-  );
-  weatherForecastIconElement.innerHTML = response.data.weather.icon;*/
-
   /*jak te dwa gówna połączyć, próbowałam qselectorAll i nie poszło*/
   let temperatureElement = document.querySelector("#temperature");
   let temperatureForecastElement = document.querySelector(
     "#temperature-forecast"
   );
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  temperatureForecastElement.innerHTML = Math.round(response.data.main.temp);
-
-  /*jak te dwa gówna połączyć, próbowałam qselectorAll i nie poszło*/
   let descriptionElement = document.querySelector("#description");
   let descriptionForecastElement = document.querySelector(
     "#description-forecast"
   );
+  let windSpeedElement = document.querySelector("#wind-speed-forecast");
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperatureForecastElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].main;
   descriptionForecastElement.innerHTML = response.data.weather[0].main;
-
-  let windSpeedElement = document.querySelector("#wind-speed-forecast");
   windSpeedElement.innerHTML = response.data.wind.speed;
+  /*
+  let weatherForecastIconElement = document.querySelector(
+    "#weather-forecast-icon"
+  );
+  weatherForecastIconElement.innerHTML = response.data.weather[0].icon;
+  
+  let precipitationForecastElement = document.querySelector(
+    "#precipitation-forecast"
+  );
+  precipitationForecastElement.innerHTML = response.data.precipitation.value;*/
 }
 
 let apiKey = "6d0dc84f33996746a53bd0932ee1515d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Sydney";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
